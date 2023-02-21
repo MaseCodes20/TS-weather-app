480954364b44d6d9f33195425af8920865cf91e5
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2023-02-21 13:12:55 -0500

    added axios and getWeather function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
+import { getWeather } from './api/weaterAPI'
 
 function App() {
+  getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
+
   return (
     <div>
       <h1 className='text-3xl font-bold'>Weather App!</h1>
---
+++ src/api/weaterAPI.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const BASE_URL = 'https://api.open-meteo.com/v1/forecast'
+
+export const getWeather = async (lat: number, lon: number, timezone: string) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=${timezone}`
+    )
+
+    console.log(response.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
