b2c4518de8ff162ae8b10a454dd6b4e8eac3d119
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2023-03-17 13:23:54 -0400

    DarkModeButton styling update

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -6,22 +6,27 @@ import { MoonIcon, SunIcon } from '@heroicons/react/24/solid'
 function DarkModeButton() {
   const [theme, setTheme] = useRecoilState(themeState)
 
-  const toggleTheme = () => {
-    if (theme === 'light') {
-      setTheme('dark')
-    } else {
-      setTheme('light')
-    }
-  }
-
   useEffect(() => {
     document.body.className = theme
   }, [theme])
 
   return (
-    <button className='absolute top-0 right-0 m-5' onClick={toggleTheme}>
-      {theme === 'light' ? <MoonIcon className='h-6' /> : <SunIcon className='h-7' />}
-    </button>
+    <div className='absolute top-0 right-0 m-5 flex items-center'>
+      <button onClick={() => setTheme('light')}>
+        {' '}
+        <SunIcon className='h-7' />
+      </button>
+      <div
+        className={`flex items-center ${
+          theme === 'dark' && 'justify-end'
+        } transition-all w-14 h-6 mx-1 bg-[#046] border-2 border-fuchsia-300 rounded-full`}
+      >
+        <div className='w-6 h-5 bg-white rounded-full'></div>
+      </div>
+      <button onClick={() => setTheme('dark')}>
+        <MoonIcon className='h-6' />
+      </button>
+    </div>
   )
 }
 
